2ccda8565b900a5f316d1f03edcc5dc52646157c
Malay <13.malayjoshi@gmail.com>
2021-07-29 14:33:25 +0530

    added

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-from flask import Flask, render_template, request
-import pickle
 import numpy as np
-
-model = pickle.load(open('iris.pkl', 'rb'))
+from flask import Flask, request, jsonify, render_template
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
-def main():
-    return render_template('home.html')
-
-
-@app.route('/predict', methods=['POST'])
 def home():
-    data1 = request.form['a']
-    data2 = request.form['b']
-    data3 = request.form['c']
-    data4 = request.form['d']
-    arr = np.array([[data1, data2, data3, data4]])
-    prediction = model.predict(arr)
-    return render_template('result.html', data=prediction)
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
+    prediction = model.predict(final_features)
+    
+
+    output = prediction[0]
+
+    return render_template('index.html', prediction_text='Class of a Iris Plant: {}'.format(output))
 
 
 if __name__ == "__main__":
